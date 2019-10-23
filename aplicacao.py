80a85874820c73b113bd181b7c487b395e10d342
DIOMAR ELIAS DE SOUZA - Claro Matriz - <92038396@corp.clarobr>
2019-10-22 22:49:32 -0300

    Rev07

--- aplicacao.py
+++ aplicacao.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask (__name__)
 
-@app.route('/index')
+@app.route('https://diomar11071985a.github.io/Projeto-Flask/')
 
 def index():
     return render_template("index.html")
